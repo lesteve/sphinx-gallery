28c97a0874e33f4d9f26545c4c3bd04d1b4715c7
Loïc Estève <loic.esteve@ymail.com>
2017-12-18 14:40:54 +0100

    Add codecov support
    Also some maintenance tasks:
    - move part of .travis.yml to separate scripts
    - use miniconda3 rather than miniconda2 installer
    - use INSTALL_MAYAVI env variable

---
+++ continuous_integration/travis/test_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script is meant to be called by the "script" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+#
+# License: 3-clause BSD
+
+set -e
+
+python setup.py test
+pytest sphinx_gallery
+cd doc
+make html-noplot
+make html -j 2
+
+cd $OLDPWD
